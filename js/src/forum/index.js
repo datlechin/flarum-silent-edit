05a9c2cede4cb832594d1b9353fa22e39d7e75a1
Ngo Quoc Dat <datlechin@gmail.com>
2026-02-24 16:45:35 +0700

    feat: migrate to Flarum 2.0
    - Update dependencies (flarum/core ^2.0.0-beta, flarum-webpack-config ^3.0.0)
    - Replace ApiSerializer with ApiResource + Schema fields
    - Migrate admin permissions to Extend.Admin() extender pattern
    - Convert to string-based extend() for PostControls
    - Migrate yarn to npm
    - Update CI workflows for 2.x

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,25 +1,20 @@
 import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
-import PostControls from 'flarum/forum/utils/PostControls';
 import Button from 'flarum/common/components/Button';
-import Post from 'flarum/common/models/Post';
-import Model from 'flarum/common/Model';
 
 app.initializers.add('datlechin/flarum-silent-edit', () => {
-  Post.prototype.canClearLastEdit = Model.attribute('canClearLastEdit');
-
-  extend(PostControls, 'moderationControls', function (items, post) {
-    if (!post.canClearLastEdit() || !post.editedUser()) return;
+  extend('flarum/forum/utils/PostControls', 'moderationControls', function (items, post) {
+    if (!post.attribute('canClearLastEdit') || !post.editedUser()) return;
 
     items.add(
       'clearLastEdit',
-      <Button icon="fas fa-volume-mute" onclick={this.clearLastEdit.bind(post)}>
+      <Button icon="fas fa-volume-mute" onclick={() => clearLastEdit(post)}>
         {app.translator.trans('datlechin-silent-edit.forum.post_controls.clear_last_edit_button')}
       </Button>
     );
   });
-
-  PostControls.clearLastEdit = function () {
-    return this.save({ isEdited: false }).then(() => m.redraw());
-  };
 });
+
+function clearLastEdit(post) {
+  return post.save({ isEdited: false }).then(() => m.redraw());
+}
